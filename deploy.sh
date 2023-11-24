80a241898466a51fb3c984e2dac3bafbb3e1eeaf
ti-ebeg <108417192+ti-ebeg@users.noreply.github.com>
2023-11-24 15:51:35 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "Instalando depedencias"
+apt update && apt upgrade -y && apt install git chromium-browser
+echo "Acessando pasta para trabalhos"
+cd /tmp/
+echo "Clonando repositórios"
+git clone https://github.com/ti-ebeg/portal-connect.git
+cd portal-connect
+echo "Tornando o script executável ..."
+chmod +x kiosk.sh
+echo "80% concluído"
+echo "Movendo arquivos para as pastas corretas ..."
+mv kiosk.sh /usr/bin/
+mv com.kiosk.desktop /usr/share/applications/
+mv com.kiosk.WindowManager.desktop /usr/share/applications/
+mv sh.com.kiosk.desktop /usr/share/xsessions/com.kiosk.desktop
+echo "90% concluído ..."
+echo "Tenha paciência meu jovem Padawan  :)"
+echo "Colocando o programa na inicialização do sistema"
+#systemctl enable kiosk.service
+echo "Bom, eu acho que é isso... Vou iniciar o programa"
+exit
+sh /usr/bin/kiosk.sh
+done
