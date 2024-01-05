3fc8599666431c5934eaaf5b345f2027982631cb
ti-ebeg <108417192+ti-ebeg@users.noreply.github.com>
2024-01-05 11:30:14 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,17 @@ echo "Acessando pasta para trabalhos"
 cd /tmp/
 echo "Clonando repositórios"
 git clone https://github.com/ti-ebeg/portal-connect.git
+echo "Acessando pasta para inicio dos trabalhos"
 cd portal-connect
-echo "Tornando o script executável ..."
+echo "Tornando o script executável ...........................[OK]"
 chmod +x kiosk.sh
-echo "80% concluído"
+echo "80% concluído ...........................[OK]"
 echo "Movendo arquivos para as pastas corretas ..."
 mv kiosk.sh /usr/bin/
 mv com.kiosk.desktop /usr/share/applications/
 mv com.kiosk.WindowManager.desktop /usr/share/applications/
 mv sh.com.kiosk.desktop /usr/share/xsessions/com.kiosk.desktop
-echo "90% concluído ..."
+echo "90% concluído ...........................[OK]"
 echo "Tenha paciência meu jovem Padawan"
 echo "Colocando o programa na inicialização do sistema"
 #systemctl enable kiosk.service
